b764f73d06febfffe1e25f3db1e4761662283d94
AlbaDuqueC <alba.duque@iesnervion.es>
2025-10-20 14:38:13 +0200

    Unidad 6 -> Ejercicio 3

---
+++ Sis/Unidad6/Ejercicio1/Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+using System.Diagnostics;
+using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Ejercicio1.Models.Entities;
+
+namespace Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            int hora = DateTime.Now.Hour;
+            string saludo;
+            Persona persona;
+
+            if (hora >= 6 && hora < 12)
+                saludo = "Buenos días";
+            else if (hora >= 12 && hora < 20)
+                saludo = "Buenas tardes";
+            else
+                saludo = "Buenas noches";
+
+            ViewData["Saludo"] = saludo;
+
+            // Fecha actual en formato largo
+            ViewBag.Fecha = DateTime.Now.ToLongDateString();
+
+            //Crear persona y pasarla por el modelo
+            persona = new Persona(1, "Alba", 19);
+
+            return View(persona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+    }
+}
---
+++ Sis/Unidad6/Ejercicio1/Ejercicio1/Models/Entities/Persona.cs
@@ -0,0 +1,52 @@
+﻿namespace Ejercicio1.Models.Entities
+{
+    public class Persona
+    {
+        #region Atributos privados
+        private int _id;
+        private string _nombre;
+        private int _edad;
+        #endregion
+        #region geters y setters
+        
+        public int id {             
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string nombre {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public int edad {
+            get { return _edad; }
+            set { _edad = value; }
+        }
+
+
+
+        #endregion
+
+        #region constructores
+        public Persona(int id, string nombre, int edad)
+        {
+            this._id = id;
+            this._nombre = nombre;
+            this._edad = edad;
+        }
+
+        public Persona()
+        {
+            this._id = 0;
+            this._nombre = "";
+            this._edad = 0;
+        }
+
+
+        #endregion
+
+
+    }
+
+}
