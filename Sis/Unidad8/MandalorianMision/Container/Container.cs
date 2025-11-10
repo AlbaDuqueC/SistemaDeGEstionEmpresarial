88e6bbcc6b068cc4797de77fc7da0d20c6fb5994
AlbaDuqueC <alba.duque@iesnervion.es>
2025-11-10 14:42:24 +0100

    Undiad 8

---
+++ Sis/Unidad8/MandalorianMision/Container/Container.cs
@@ -0,0 +1,13 @@
+﻿namespace Container
+{
+    public class Container
+    {
+        public static IServiceCollection AddCompositionRoot (this IServiceCollection services, IConfiguration configuration)
+        {
+           services.AddScoped<IRepositoryMision, RepositoryMision>();
+            servicesAddScoped<IUseCaseListadoMisiones, ListadoMisionesUseCase>();
+
+            return services;
+        }
+    }
+}
---
+++ Sis/Unidad8/MandalorianMision/Domain/Entities/Mision.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    internal class Mision
+    {
+
+        private int _id;
+        private String _nombre;
+        private String _descripcion;
+        private int _recompensa;
+
+
+        public Mision(int id, String nombre, String descripcion, int recompensa)
+        {
+            _id = id;
+            _nombre = nombre;
+            _descripcion = descripcion;
+            _recompensa = recompensa;
+        }
+        public Mision()
+        {
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _id;
+            }
+
+        }
+
+        public String Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;
+            }
+        }
+        public String Descripcion
+        {
+            get
+            {
+                return _descripcion;
+            }
+            set
+            {
+                _descripcion = value;
+            }
+        }
+        public int Recompensa
+        {
+            get
+            {
+                return _recompensa;
+            }
+            set
+            {
+                _recompensa = value;
+            }
+
+        }
+    }
+}
