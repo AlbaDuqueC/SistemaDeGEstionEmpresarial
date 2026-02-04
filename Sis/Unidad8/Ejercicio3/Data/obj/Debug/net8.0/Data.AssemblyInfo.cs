5aedfe9dff9eee19915242fedebfe49862adfd87
AlbaDuqueC <alba.duque@iesnervion.es>
2026-02-04 11:40:26 +0100

    ns, me lo acabo de encontrar así

--- Sis/Unidad8/Ejercicio3/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Sis/Unidad8/Ejercicio3/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b26ac483bee901706a9cec2bf6228522befe4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7fc65b078ee140aac70cdca93debd8c16b9af93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Sis/Unidad8/Ejercicio3/Ejercicio3/obj/Debug/net8.0/Ejercicio3.AssemblyInfo.cs
+++ Sis/Unidad8/Ejercicio3/Ejercicio3/obj/Debug/net8.0/Ejercicio3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ejercicio3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8b26ac483bee901706a9cec2bf6228522befe4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7fc65b078ee140aac70cdca93debd8c16b9af93")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ejercicio3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ejercicio3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
